402ae2af53290b079130b9297a4c5027a5308a46
Samip Shrestha <javasamip@gmail.com>
2017-07-31 13:25:10 +0545

    changes in main.js

--- js/main.js
+++ js/main.js
@@ -1,37 +1,35 @@
 ;
-(function () {
+(function() {
     'use strict';
     var options1 = {
-        multipleDate: false,
+        multipleDate: true,
         minDate: "2017-06-19",
-        preDates: ['2017-06-18', '2017-06-1', '2017-06-19','2017-06-20'], //yyyy-mm-dd
-//        showPreDates: false,
-        showCurrentDateButton: true
-        , activation: 'alwaysOn'
-        , singleCalendarMode: false
-        , showCurrentDate: true
-        , callback: function (date, activedates) {
+        preDates: ['2017-06-18', '2017-06-1', '2017-06-19', '2017-06-20'], //yyyy-mm-dd
+        //        showPreDates: false,
+        showCurrentDateButton: true,
+        activation: 'alwaysOn',
+        singleCalendarMode: false,
+        showCurrentDate: true,
+        callback: function(date, activedates) {
             console.log(date);
             console.log(activedates);
-        }
-        , setWeekHoliday: true
-        , holiday: 'saturday'
-        , disableClickToggle: true
-//        , defaultViewMonth: 10
-//        , defaultViewYear: 2016
+        },
+        setWeekHoliday: true,
+        holiday: 'saturday',
+        disableClickToggle: false
+            //        , defaultViewMonth: 10
+            //        , defaultViewYear: 2016
             //animation: 'flipInY',
     }
     var options2 = {
-        multipleDate: false
-        , minDate: '2017-3-18', // yyyy-mm-dd
-        maxDate: '2017-5-20', // yyyy-mm-dd
+        multipleDate: false,
+        minDate: '2017-3-18', // yyyy-mm-dd
+        //maxDate: '2017-5-20', // yyyy-mm-dd
         // animation: 'flipInY',
-        activation: 'onClick'
-        , // format: 'D MMMM, YYYY'	
-        showCurrentDateButton: false
-        , singleCalendarMode: false
-        , disableClickEvent: false
-        , // disableClickToggle: true
+        activation: 'onClick', // format: 'D MMMM, YYYY'	
+        showCurrentDateButton: false,
+        singleCalendarMode: false,
+        disableClickEvent: false, // disableClickToggle: true
         // format: 'D MMMM, YYYY'	
     }
     $('#input-date').datePicky(options2);
